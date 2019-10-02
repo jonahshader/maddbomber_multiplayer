4fbe734b9a5f56dcc8bc12627bade00e6f54df06
Jonah Shader <jonah.shader@gmail.com>
2019-10-02 15:16:10 -0400

    started porting over original maddbomber code

--- core/src/jonahshader/game/MainGame.kt
+++ core/src/jonahshader/game/MainGame.kt
@@ -1,30 +1,26 @@
 package jonahshader.game
 
 import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 
 class MainGame : ApplicationAdapter() {
+    companion object {
+        const val V_WIDTH = 640
+        const val V_HEIGHT = 360
+        const val TILE_SIZE = 32
+    }
+
     private lateinit var batch: SpriteBatch
-    private lateinit var img: Texture
+    private lateinit var shapeRenderer: ShapeRenderer
 
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
-    }
-
-    override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
+        shapeRenderer = ShapeRenderer()
     }
 
     override fun dispose() {
         batch.dispose()
-        img.dispose()
+        shapeRenderer.dispose()
     }
 }
